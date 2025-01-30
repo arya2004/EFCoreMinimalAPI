926747b9aec3220f58036c80e1a4f3539e32f79c
arya2004 <arya20j4@gmail.com>
2025-01-30 23:13:32 +0530

    Add AppDbContext with custom Identity table names

---
+++ EFCoreMinimalAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMinimalAPI.Data
+{
+   
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+        }
+    }
+}
